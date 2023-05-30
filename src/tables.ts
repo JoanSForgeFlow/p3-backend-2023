0164dfc6c3956fb81847649ab8fe0f0a1a192769
Joan Sisquella <joan.sisquella@forgeflow.com>
2023-05-30 15:33:33 +0200

    [ADD] POST PUT DELETE Restaurant Tables

--- src/tables.ts
+++ src/tables.ts
@@ -34,4 +34,41 @@ router.get(
   })
 );
 
+router.post(
+  "/",
+  errorChecked(async (req, res) => {
+    const newTable = await prisma.table.create({ data: req.body });
+    res.status(201).json({ newTable });
+  })
+);
+
+router.put(
+  "/:id",
+  errorChecked(async (req, res) => {
+    const id = Number(req.params.id);
+    const table = await prisma.table.findUnique({
+      where: { id },
+    });
+    if (!table) {
+      return res.status(404).json({ error: `Table with ID ${id} not found` });
+    }
+    const updatedTable = await prisma.table.update({
+      where: { id },
+      data: req.body,
+    });
+    res.status(200).json({ updatedTable });
+  })
+);
+
+router.delete(
+  "/:id",
+  errorChecked(async (req, res) => {
+    const id = Number(req.params.id);
+    const deletedTable = await prisma.table.delete({
+      where: { id },
+    });
+    res.status(200).json({ deletedTable });
+  })
+);
+
 export default router;
