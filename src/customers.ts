1b960412515575bb28074d431e36387675567f22
Joan Sisquella <joan.sisquella@forgeflow.com>
2023-05-30 17:28:04 +0200

    [ADD] Customers POST PUT and DELETE

--- src/customers.ts
+++ src/customers.ts
@@ -26,4 +26,50 @@ router.get(
   })
 );
 
+router.post(
+  "/",
+  errorChecked(async (req, res) => {
+    const newCustomer = await prisma.customer.create({
+      data: req.body,
+    });
+    res.status(201).json({ customer: newCustomer });
+  })
+);
+
+router.put(
+  "/:id",
+  errorChecked(async (req, res) => {
+    const id = Number(req.params.id);
+    const customer = await prisma.customer.findUnique({
+      where: { id },
+    });
+    if (!customer) {
+      return res.status(404).json({ error: `Customer with ID ${id} not found` });
+    }
+    const updatedCustomer = await prisma.customer.update({
+      where: { id },
+      data: req.body,
+    });
+    res.status(200).json({ customer: updatedCustomer });
+  })
+);
+
+router.delete(
+  "/:id",
+  errorChecked(async (req, res) => {
+    const id = Number(req.params.id);
+    const customer = await prisma.customer.findUnique({
+      where: { id },
+    });
+    if (!customer) {
+      return res.status(404).json({ error: `Customer with ID ${id} not found` });
+    }
+    const deletedCustomer = await prisma.customer.delete({
+      where: { id },
+    });
+    res.status(200).json({ customer: deletedCustomer });
+  })
+);
+
+
 export default router;
