bb85d52720a5bd3920683a07bbfa37a3e3e754a5
Joan Sisquella <joan.sisquella@forgeflow.com>
2023-05-31 21:56:17 +0200

    [IMP] seed.ts with more data and diversity

--- src/seed.ts
+++ src/seed.ts
@@ -1,119 +1,193 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 import moment from 'moment';
+
 async function main() {
-await prisma.booking.deleteMany();
-await prisma.review.deleteMany();
-await prisma.customer.deleteMany();
-await prisma.table.deleteMany();
-await prisma.restaurant.deleteMany();
+  await prisma.booking.deleteMany();
+  await prisma.review.deleteMany();
+  await prisma.customer.deleteMany();
+  await prisma.table.deleteMany();
+  await prisma.restaurant.deleteMany();
 
-const r1 = await prisma.restaurant.create({
-  data: { 
-    name: "Restaurant 1", 
-    location: "Location 1",
-    cuisine: "Cuisine 1"
-  },
-});
-console.log(`Created restaurant ${r1.name} (${r1.id})`);
+  const restaurant1 = await prisma.restaurant.create({
+    data: {
+      name: "Chez Pierre",
+      location: "New York",
+      cuisine: "French",
+    },
+  });
 
-const r2 = await prisma.restaurant.create({
-  data: { 
-    name: "Restaurant 2", 
-    location: "Location 2",
-    cuisine: "Cuisine 2"
-  },
-});
-console.log(`Created restaurant ${r2.name} (${r2.id})`);
+  const restaurant2 = await prisma.restaurant.create({
+    data: {
+      name: "Bella Napoli",
+      location: "Chicago",
+      cuisine: "Italian",
+    },
+  });
 
-const t1 = await prisma.table.create({
+  // Creating tables for Restaurant 1
+  const table1 = await prisma.table.create({
+    data: {
+      restaurantId: restaurant1.id,
+      number: 1,
+      capacity: 4,
+    },
+  });
+
+  const table2 = await prisma.table.create({
+    data: {
+      restaurantId: restaurant1.id,
+      number: 2,
+      capacity: 6,
+    },
+  });
+
+  // Creating tables for Restaurant 2
+  const table3 = await prisma.table.create({
+    data: {
+      restaurantId: restaurant2.id,
+      number: 1,
+      capacity: 5,
+    },
+  });
+
+  const table4 = await prisma.table.create({
+    data: {
+      restaurantId: restaurant2.id,
+      number: 2,
+      capacity: 7,
+    },
+  });
+
+  const customer1 = await prisma.customer.create({
+    data: {
+      name: "John Doe",
+      email: "johndoe@example.com",
+      phone: "123-456-7890"
+    },
+  });
+
+  const customer2 = await prisma.customer.create({
+    data: {
+      name: "Jane Smith",
+      email: "janesmith@example.com",
+      phone: "098-765-4321"
+    },
+  });
+
+  const customer3 = await prisma.customer.create({
+    data: {
+      name: "Alice Johnson",
+      email: "alicejohnson@example.com",
+      phone: "555-555-5555"
+    },
+  });
+
+  // Booking and reviews for customer1
+  const bookingDateTime1 = moment.utc(`2023-01-01T19:00:00.000Z`);
+  const bookingEndDateTime1 = moment.utc(bookingDateTime1).add(2, 'hours');
+
+  await prisma.booking.create({
+    data: {
+      customerId: customer1.id,
+      tableId: 1,
+      restaurantId: restaurant1.id,
+      bookingDate: bookingDateTime1.toDate(),
+      bookingEndTime: bookingEndDateTime1.toDate(),
+      numberOfPeople: 2,
+    },
+  });
+
+  await prisma.review.create({
+    data: {
+      customerId: customer1.id,
+      restaurantId: restaurant1.id,
+      rating: 5,
+      comment: "Fantastic place! Great food and service. Highly recommended.",
+      date: new Date(),
+    },
+  });
+
+  await prisma.review.create({
+    data: {
+      customerId: customer1.id,
+      restaurantId: restaurant2.id,
+      rating: 4,
+      comment: "Wonderful service, but the food could have been hotter.",
+      date: new Date(),
+    },
+  });
+
+  // Booking and reviews for customer2
+  const bookingDateTime2 = moment.utc(`2023-02-01T20:00:00.000Z`);
+  const bookingEndDateTime2 = moment.utc(bookingDateTime2).add(2, 'hours');
+
+  await prisma.booking.create({
+    data: {
+      customerId: customer2.id,
+      tableId: 2,
+      restaurantId: restaurant2.id,
+      bookingDate: bookingDateTime2.toDate(),
+      bookingEndTime: bookingEndDateTime2.toDate(),
+      numberOfPeople: 4,
+    },
+  });
+
+  await prisma.review.create({
+    data: {
+      customerId: customer2.id,
+      restaurantId: restaurant2.id,
+      rating: 4,
+      comment: "Nice ambience, good food but a bit pricey.",
+      date: new Date(),
+    },
+  });
+
+  await prisma.review.create({
+    data: {
+      customerId: customer2.id,
+      restaurantId: restaurant1.id,
+      rating: 3,
+      comment: "Service was good but the food lacked seasoning.",
+      date: new Date(),
+    },
+  });
+
+// Booking and reviews for customer3
+const bookingDateTime3 = moment.utc(`2023-03-01T19:30:00.000Z`);
+const bookingEndDateTime3 = moment.utc(bookingDateTime3).add(2, 'hours');
+
+await prisma.booking.create({
   data: {
-    restaurantId: r1.id,
-    number: 1,
-    capacity: 4
+    customerId: customer3.id,
+    tableId: 3,
+    restaurantId: restaurant1.id,
+    bookingDate: bookingDateTime3.toDate(),
+    bookingEndTime: bookingEndDateTime3.toDate(),
+    numberOfPeople: 3,
   },
 });
-console.log(`Created table ${t1.number} (${t1.id}) for restaurant ${r1.id}`);
 
-const t2 = await prisma.table.create({
+await prisma.review.create({
   data: {
-    restaurantId: r2.id,
-    number: 1,
-    capacity: 6
-  },
-});
-console.log(`Created table ${t2.number} (${t2.id}) for restaurant ${r2.id}`);
-
-
-const c1 = await prisma.customer.create({
-  data: {
-    name: "Customer 1",
-    email: "customer1@example.com",
-    phone: "123-456-7890"
-  },
-});
-console.log(`Created customer ${c1.name} (${c1.id})`);
-
-const c2 = await prisma.customer.create({
-  data: {
-    name: "Customer 2",
-    email: "customer2@example.com",
-    phone: "098-765-4321"
-  },
-});
-console.log(`Created customer ${c2.name} (${c2.id})`);
-
-const bookingDateTime1 = moment.utc(`2023-01-01T19:00:00.000Z`);
-const bookingEndDateTime1 = moment.utc(bookingDateTime1).add(2, 'hours');
-
-const b1 = await prisma.booking.create({
-  data: {
-    customerId: c1.id,
-    tableId: t1.id,
-    restaurantId: r1.id,
-    bookingDate: bookingDateTime1.toDate(),
-    bookingEndTime: bookingEndDateTime1.toDate(),
-    numberOfPeople: 2,
-  },
-});
-console.log(`Created booking for customer ${c1.name} (${c1.id}) at restaurant ${r1.name} (${r1.id})`);
-
-const bookingDateTime2 = moment.utc(`2023-01-01T20:00:00.000Z`);
-const bookingEndDateTime2 = moment.utc(bookingDateTime2).add(2, 'hours');
-
-const b2 = await prisma.booking.create({
-  data: {
-    customerId: c2.id,
-    tableId: t2.id,
-    restaurantId: r2.id,
-    bookingDate: bookingDateTime2.toDate(),
-    bookingEndTime: bookingEndDateTime2.toDate(),
-    numberOfPeople: 4,
-  },
-});
-console.log(`Created booking for customer ${c2.name} (${c2.id}) at restaurant ${r2.name} (${r2.id})`);
-
-const rev1 = await prisma.review.create({
-  data: {
-    customerId: c1.id,
-    restaurantId: r1.id,
+    customerId: customer3.id,
+    restaurantId: restaurant1.id,
     rating: 5,
-    comment: "Excellent service and food!",
+    comment: "Amazing experience! Loved the food and the service.",
     date: new Date(),
   },
 });
-console.log(`Created review by customer ${c1.name} (${c1.id}) for restaurant ${r1.name} (${r1.id})`);
 
-const rev2 = await prisma.review.create({
+await prisma.review.create({
   data: {
-    customerId: c2.id,
-    restaurantId: r2.id,
+    customerId: customer3.id,
+    restaurantId: restaurant2.id,
     rating: 4,
-    comment: "Great atmosphere and friendly staff!",
+    comment: "Nice atmosphere, but the menu lacks vegetarian options.",
     date: new Date(),
   },
 });
-console.log(`Created review by customer ${c2.name} (${c2.id}) for restaurant ${r2.name} (${r2.id})`);
+
 }
 
 main()
